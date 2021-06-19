de37f2d24711c3b3e2bfcce5d29e76b5e2a020f1
ce1b2964-1dc0-4ce7-b193-1dad6c164817 <shes990106@gmail.com>
2021-06-19 14:14:18 +0800

    end

--- api.py
+++ api.py
@@ -7,10 +7,8 @@ from time import time
 from flask import Flask, request, jsonify, _request_ctx_stack
 import torchvision.transforms as transforms
 import datetime
-import torch
-# torch.multiprocessing.set_start_method('spawn', force = True)
+import torch, timm
 from torch.multiprocessing import Process, Queue
-from os import getpid
 
 app = Flask(__name__)
 
@@ -23,13 +21,12 @@ def save_image(que):
     while True:
         name, image_b64, prediction = que.get(True)
         image = Image.open(BytesIO(b64decode(image_b64)))
-        image.save('public4/' + name + '_' + prediction + '.jpg')
+        image.save('private4/' + name + '_' + prediction + '.jpg')
 
 @app.route('/inference', methods=['POST'])
 def inference():
     """ API that return your model predictions when E.SUN calls this API. """
     data = request.get_json(force = True)
-    print('get', time() - data['esun_timestamp'])
 
     with torch.no_grad():
         torch.cuda.empty_cache()
@@ -48,7 +45,7 @@ def inference():
     s = sha256()
     s.update((CAPTAIN_EMAIL + ts + SALT).encode("utf-8"))
     server_uuid = s.hexdigest()
-    print('end', prediction, time() - data['esun_timestamp'])
+
     return jsonify({'esun_uuid': data['esun_uuid'],
                     'server_uuid': server_uuid,
                     'answer': prediction,
@@ -62,14 +59,14 @@ if __name__ == "__main__":
     )
     arg_parser.add_argument('-p', '--port', default=5000, help='port')
     arg_parser.add_argument('-d', '--debug', default=True, help='debug')
+    arg_parser.add_argument('-sz', '--size', default=448)
+    arg_parser.add_argument('-n', '--n_thread', default=4)
     options = arg_parser.parse_args()
 
     image_storage = Queue()
     model_usage = Queue()
-    model_usage.put(0)
-    model_usage.put(1)
-    model_usage.put(2)
-    model_usage.put(3)
+    for i in range(options.n_thread):
+        model_usage.put(i)
     
     with open('class.txt', 'r', encoding = 'utf8') as file:
         classes = file.read().splitlines()
@@ -77,27 +74,14 @@ if __name__ == "__main__":
 
 
     torch.backends.cudnn.benchmark = True
-    with torch.no_grad():
-        test = torch.zeros((1, 3, 448, 448)).half().cuda()
-        model = [torch.load('eff_b8_448.weight') for _ in range(4)]
-        # model = [torch.jit.trace(torch.load('eff_b8_448.weight'), test, strict = False) for _ in range(4)]
-        del test
+    model = [torch.load('tmp.weight').half() for _ in range(options.n_thread)]
     for m in model:
         m.eval()
-    # model.eval()
+
     test_transform = torch.nn.Sequential(
         transforms.Pad((60, 30), fill = 0.941176),
         transforms.CenterCrop((67, 100)),
         transforms.Resize((448, 448)),
-        transforms.Normalize((0.3812, 0.3593, 0.3634), (0.3999, 0.3854, 0.3828), inplace = True),
-    )
-    addition_transform = torch.nn.Sequential(
-        transforms.Pad((5, 5), fill = 0.941176),
-        transforms.RandomRotation(10, fill = 0.941176),
-        transforms.RandomPerspective(0.1, fill = 0.941176),
-        transforms.RandomCrop((67, 100), fill = 0.941176, pad_if_needed = True),
-        transforms.Resize((448, 448)),
-        transforms.ColorJitter(brightness = (0.8, 1.2), saturation = (0.8, 1.2), contrast = (0.8, 1.2), hue = 0.05),
         transforms.Normalize((0.3812, 0.3593, 0.3634), (0.3999, 0.3854, 0.3828), inplace = True),
     )
 
--- train.py
+++ train.py
@@ -6,27 +6,35 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import timm
 import torch.optim as optim 
-from torchvision.models import resnext101_32x8d
 from time import time
+import datetime
+from transformers import AdamW
 
 class HCCDataset(Dataset):
-    def __init__(self, DataList, transform, mode = True):
+    def __init__(self, DataList, transform, NULLData = None):
         super(HCCDataset, self).__init__()
         self.DataList = DataList
         self.transform = transform
         self.ToTensor = transforms.ToTensor()
         self.hflip = transforms.functional.hflip
+        self.NULLData = NULLData
     def __len__(self):
         return len(self.DataList)
     def __getitem__(self, index):
-        label = int(self.DataList[index][self.DataList[index].index('_') + 1 : -4])
+        file = self.DataList[index]
+        if self.NULLData and torch.rand(size = (1,)) > 0.95:
+            file = self.NULLData[torch.randint(0, len(self.NULLData), size = (1,))]
 
-        if self.DataList[index][-3:] == 'jpg':
-            return self.transform(self.ToTensor(Image.open('../train2/' + self.DataList[index]))), label
+        label = int(file[file.index('_') + 1 : -4])
+
+        if file[-3:] == 'jpg':
+            return self.transform(self.ToTensor(Image.open('../train2/' + file))), label
+        elif label < 800:
+            return self.transform(self.ToTensor(Image.open('../pretrain/' + file))), label
         else:
-            return self.transform(self.ToTensor(Image.open('../pretrain/' + self.DataList[index]))), label
-
+            return self.transform(self.ToTensor(Image.open('../pretrain2/' + file))), label
 def forward(DataLoader, model, LossFunction, optimizer = None):
+
     correct = 0
     TotalLoss = 0
     cases = 0
@@ -37,21 +45,25 @@ def forward(DataLoader, model, LossFunction, optimizer = None):
         if optimizer:
             optimizer.zero_grad()
 
-        inputs = inputs.cuda()
+        inputs = inputs.half().cuda()
         with torch.cuda.amp.autocast():
             labels = labels.cuda()
             outputs = model(inputs)
             del inputs
             loss = LossFunction(outputs, labels)
+
         if loss.item() != loss.item():
-            print('GG')
+            print('NAN')
+            exit()
         TotalLoss += loss.item() * labels.shape[0]
         if optimizer:
             loss.backward()
             optimizer.step()
+
         pred = outputs.argmax(1)
         cases += labels.shape[0]
         correct += (pred == labels).sum().item()
+
         del outputs, pred, loss, labels
         print('%5d/%5d'%(index + 1, DataLoader.__len__()), flush = True, end = '\b' * 11)
 
@@ -66,20 +78,25 @@ if __name__ == '__main__':
     parser.add_argument("--ep", type = int, default = 1000)
     parser.add_argument("--bs", type = int, default = 64)
     parser.add_argument("--sz", type = int, default = 224)
+    parser.add_argument("--seed", type = int, default = 1234)
     parser.add_argument("--lr", type = float, default = 0.0001)
     parser.add_argument("--sv", type = str, default = 'tmp.weight')
-    parser.add_argument("--ld", type = str, default = None) #cait_m48_448
+    parser.add_argument("--ld", type = str, default = None)
     parser.add_argument("--version", type = str)
     args = parser.parse_args()
     print(args)
 
-    # with open('class.txt', 'r', encoding = 'utf8') as file:
-    #   classes = file.read().splitlines()
-
     DataList = list(os.walk('../train2/'))[0][2]
     AdditionalData = list(os.walk('../pretrain/'))[0][2]
-    seed(999)
+
+    TestData = list(os.walk('../test/'))[0][2]
+    for i in range(len(TestData)):
+        TestData[i] = '../test/' + TestData[i]
+    AdditionalNULLData = list(os.walk('../pretrain2/'))[0][2]
+
+    seed(args.seed)
     shuffle(DataList)
+    shuffle(AdditionalNULLData)
 
 
     train_transform = torch.nn.Sequential(
@@ -101,28 +118,30 @@ if __name__ == '__main__':
     )
 
 
-    TrainingSet = HCCDataset(DataList[:-7000] + AdditionalData, train_transform)
+    TrainingSet = HCCDataset(DataList[:-7000] + TestData, train_transform)
     ValidationSet = HCCDataset(DataList[-7000:], test_transform)
+    NULLSet = HCCDataset(AdditionalNULLData[-13000:], test_transform)
     print(TrainingSet.__len__(), ValidationSet.__len__())
 
     TrainingLoader = DataLoader(TrainingSet, batch_size = args.bs, shuffle = True, pin_memory = True, drop_last = True, num_workers = 4)
-    ValidationLoader = DataLoader(ValidationSet, batch_size = 128, pin_memory = True, num_workers = 4)
+    ValidationLoader = DataLoader(ValidationSet, batch_size = 64, pin_memory = True, num_workers = 4)
+    NULLLoader = DataLoader(NULLSet, batch_size = 64, pin_memory = True, num_workers = 4)
     if args.ld:
         model = torch.load(args.ld)
     else:
-        model = timm.create_model(args.version, pretrained = True, num_classes = 801).half().cuda()
-        model.reset_classifier(801)
+        model = timm.create_model(args.version, pretrained = True, num_classes = 801)
         model = model.cuda()
         # model = torch.nn.DataParallel(model)
     torch.backends.cudnn.benchmark = True
-    # optimizer = AdamW(model.parameters(), lr = args.lr, eps = 1e-4)
+    # optimizer = AdamW(model.parameters(), lr = args.lr)
     optimizer = torch.optim.Adam(model.parameters(), lr = args.lr)
     LossFunction = torch.nn.CrossEntropyLoss()
 
     model.eval()
-    print('Validation', end = ' ')
+    
     with torch.no_grad():
         if args.ld:
+            print('Validation', end = ' ')
             t = time()
             BestLoss = forward(ValidationLoader, model, LossFunction)
             print('Time %5d sec'%(time() - t))
@@ -142,6 +161,10 @@ if __name__ == '__main__':
         with torch.no_grad():
             t = time()
             loss = forward(ValidationLoader, model, LossFunction)
+            print('Time %5d sec'%(time() - t))
+            print('NULL', end = ' ')
+            t = time()
+            forward(NULLLoader, model, LossFunction)
             print('Time %5d sec'%(time() - t))
         
         if loss < BestLoss:
